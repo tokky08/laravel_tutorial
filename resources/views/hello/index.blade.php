466a936e281e7ad95c89f47a984be4ff918b2aa2
TOKITA KAZUHITO <tokky.kz08@gmail.com>
2020-04-03 00:33:39 +0900

    @component

---
+++ resources/views/hello/index.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.helloapp')
+
+@section('title', 'Index')
+
+@section('menubar')
+    @parent
+    インデックスページ
+@endsection
+
+@section('content')
+    <p>ここが本文のコンテンツです。</p>
+    <p>必要なだけ記述できます。</p>
+
+    @component('components.message')
+        @slot('msg_title')
+        CAUTION!
+        @endslot
+
+        @slot('msg_content')
+        これはメッセージの表示です。
+        @endslot
+    @endcomponent
+    
+@endsection
+
+@section('footer')
+copyright 2020 tokky08
+@endsection
